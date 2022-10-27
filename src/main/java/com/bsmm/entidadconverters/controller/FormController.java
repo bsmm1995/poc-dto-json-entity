47a17aaf621558c788b746ea0546b63cdad5c4ca
Bladimir Minga <bladiminga46@gmail.com>
2022-10-27 10:45:46 -0500

    Se cambia contexto de FormrsController

--- src/main/java/com/bsmm/entidadconverters/controller/FormController.java
+++ src/main/java/com/bsmm/entidadconverters/controller/FormController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/persons")
+@RequestMapping("/forms")
 @RequiredArgsConstructor
 public class FormController {
     private final FormService formService;
